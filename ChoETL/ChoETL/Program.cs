8996fa9329d22f80ac4db8d77304a344eb678e75
John Koz <john.kozlowski@cunamutual.com>
2018-02-20 13:00:57 -0600

    Initial Commit

---
+++ ChoETL/ChoETL/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//From URL: https://www.codeproject.com/Articles/1145337/Cinchoo-ETL-CSV-Reader#1.Introduction0
+
+namespace ChoETL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (dynamic e in new ChoCSVReader("Emp.csv").WithFirstLineHeader())
+            {
+                Console.WriteLine(string.Format("ID: {0}", e.ID));
+                Console.WriteLine(string.Format("Name: {1}", e.Name));
+            }
+        }
+    }
+}
